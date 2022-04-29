2a0476b9cb2e162b5a55792b46dbbfd09c1352ec
xiongmao1512 <104600730+xiongmao1512@users.noreply.github.com>
2022-04-29 11:47:54 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -39,7 +39,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/🌞 Private/💰付费课程'],
+  protectedRoutes: ['/👉🏻往下点/03💰付费课程'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'xmao1512@gmail.com',
